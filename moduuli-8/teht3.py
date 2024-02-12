ef671ac210045344a26a0053f8a222f2c93d008c
Riku Kaartoaho <rikukaartoaho@gmail.com>
2024-02-12 19:50:57 +0200

    finish module 8

--- moduuli-8/teht3.py
+++ moduuli-8/teht3.py
@@ -6,3 +6,37 @@ Laske etäisyys geopy-kirjaston avulla: https://geopy.readthedocs.io/en/stable/.
 Asenna kirjasto valitsemalla View / Tool Windows / Python Packages.
 Kirjoita hakukenttään geopy ja vie asennus loppuun.
 '''
+
+import mysql.connector
+from geopy.distance import geodesic
+
+
+def fetch_data(a, b):
+    sql = f"SELECT latitude_deg, longitude_deg FROM airport WHERE airport.ident = '{a}' OR airport.ident = '{b}'"
+    cursor = db_connection.cursor()
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    calc_dist(res)
+
+
+def calc_dist(res):
+    a = (res[0][0], res[0][1])
+    b = (res[1][0], res[1][1])
+
+    dist = geodesic(a, b).kilometers
+    print("Lentokenttien etäisyys: ", round(dist), "km")
+
+
+db_connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port="3306",
+    database="flight_game",
+    user="root",
+    password="admin",
+    autocommit=True
+)
+
+first = input("Syötä ensimmäisen lentokentän ICAO-tunnus: ")
+second = input("Syötä toisen lentokentän ICAO-tunnus: ")
+
+fetch_data(first, second)
